6b198fa3fe877b1ea07ddf511b3b08dd54034c05
vishnu3413 <151937795+vishnu3413@users.noreply.github.com>
2023-11-29 15:20:08 +0530

    Files Upload

---
+++ client.js
@@ -0,0 +1,43 @@
+const socket = io("http://localhost:8000", { transports: ["websocket"] });
+
+const form = document.getElementById("send-container");
+const messageinput = document.getElementById("messageinp");
+const messagecontainer = document.querySelector(".container");
+
+var audio = new Audio("tone.mp3");
+const append = (message, position) => {
+  const messageelement = document.createElement("div");
+  messageelement.innerText = message;
+  messageelement.classList.add("message");
+  messageelement.classList.add(position);
+  messagecontainer.append(messageelement);
+  if (position == "left") {
+    audio.play();
+    document.title = "New Message!";
+    setTimeout(() => {
+      document.title = "RealTime Chat-Application";
+    }, 5000);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageinput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageinput.value = "";
+});
+
+const name = prompt("Enter your name");
+socket.emit("new-user-joined", name);
+
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "left");
+});
+socket.on("receive", (data) => {
+  console.log("Recieved");
+  append(`${data.name}:${data.message}`, "left");
+});
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "left");
+});
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const { log } = require("console");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log(name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    console.log("Sent");
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
